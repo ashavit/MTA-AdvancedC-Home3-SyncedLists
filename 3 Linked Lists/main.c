9a16b65f27db467ee29c529e1a1857745e173572
Amir Shavit <amir.shavit@autodesk.com>
2016-12-11 09:32:49 +0200

    Initial Commit

---
+++ 3 Linked Lists/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  3 Linked Lists
+//
+//  Created by Amir Shavit on 11/12/2016.
+//  Copyright © 2016 Amir Shavit. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
