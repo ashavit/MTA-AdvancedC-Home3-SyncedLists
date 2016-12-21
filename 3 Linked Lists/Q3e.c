47118fdfc843decf2e44feb76ea9a200c73fbf6a
Amir Shavit <amir.shavit@autodesk.com>
2016-12-21 21:57:32 +0200

    Indent main E

--- 3 Linked Lists/Q3e.c
+++ 3 Linked Lists/Q3e.c
@@ -59,32 +59,30 @@ void printList(List list);
 
 int main()
 {
-   List coordList;
+		List coordList;
 
-   int x, y;
-   int res;
+		int x, y;
+		int res;
 
-   coordList = getCoordList();
-   printList(coordList);
-   // get the (x,y) to remove
-   scanf("%d%d", &x, &y);
+		coordList = getCoordList();
 
-   res = removeCoordinate(&coordList, x, y);
+		// get the (x,y) to remove
+		scanf("%d%d", &x, &y);
 
-   if(res == 1)
-       printf("The point (%d,%d) didn't appear\n",x,y);
-   else if(res == 2)
-       printf("The point (%d,%d) has another occurrence\n",x,y);
-   else if(res == 3)
-       printf("The point (%d,%d) was the only point with this x\n",x,y);
-   else
-       printf("Other\n");
+		res = removeCoordinate(&coordList, x, y);
 
-   printList(coordList);
+		if(res == 1)
+				printf("The point (%d,%d) didn't appear\n",x,y);
+		else if(res == 2)
+				printf("The point (%d,%d) has another occurrence\n",x,y);
+		else if(res == 3)
+				printf("The point (%d,%d) was the only point with this x\n",x,y);
+		else
+				printf("Other\n");
 
-   freeList(&coordList);
+		freeList(&coordList);
 
-   return 0;
+		return 0;
 }
 
 #pragma mark - Helpers
